36b6e1609a458bfecccbb911127cc1adf86b65d2
Microfang <357147360@163.com>
2017-10-03 11:27:28 +0000

    finish layout and routes

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,14 @@
 @extends('layouts.default')
 @section('content')
-<h1>主页</h1>
+<div class="jumbotron">
+    <h1>Hello laravel</h1>
+    <p class="lead">
+        你现在所看到的是 <a href="https://fsdhub.com/books/laravel-essential-training-5.1">laravel入门教程</a>
+        的主页
+    </p>
+    <p>一切，将从这里开始</p>
+    <p>
+        <a href="{{ route('singup') }}" class="btn btn-lg btn-success" role="button">现在注册</a>
+    </p>
+</div>
 @stop
