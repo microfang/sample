0c865b58b0d9b9f8a9ee7cfd9ef66ca386786cbc
Microfang <357147360@163.com>
2017-10-05 08:47:22 +0000

    Finish user singup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,10 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Models\User;
 
 class UsersController extends Controller
 {
-    public function create(){
+    public function create()
+    {
         return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:225',
+            'password' => 'required'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        session()->flash('success','欢迎，您开启一段新旅程');
+        return redirect()->route('users.show',[$user]);
     }
 }
---
+++ resources/views/shared/_user_info.blade.php
@@ -0,0 +1,8 @@
+<style media="screen">
+/* sidebar */
+
+</style>
+<a href="{{ route('users.show', $user->id) }}">
+  <img src="{{ $user->gravatar('100') }}" alt="{{ $user->name }}" class="gravatar"/>
+</a>
+<h1>{{ $user->name }}</h1>
